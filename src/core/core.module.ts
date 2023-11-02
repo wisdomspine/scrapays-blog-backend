e2f2a91d13150d7031da206deef9f82b124c3898
Priest Ombugadu <sabopriest@gmail.com>
2023-11-02 13:17:53 +0100

    fix: validation exception factory not returning fields error

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,5 +1,11 @@
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
-import { Module, ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Module,
+  ValidationPipe,
+} from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -22,6 +28,26 @@ import { AppConfigModule, AppConfigService } from 'app-config';
       provide: APP_PIPE,
       useValue: new ValidationPipe({
         transform: true,
+        /**
+         *
+         * @param errors the errors from class-validator
+         * @returns An object, where keys are properties of object being validated, and value is the first error encountered on that property
+         */
+        exceptionFactory: (errors) => {
+          const mappedError: { [field: string]: string } = {};
+          for (const error of errors) {
+            mappedError[error.property] = Object.entries(
+              error.constraints,
+            )[0][1];
+          }
+          return new HttpException(
+            {
+              message: mappedError,
+              statusCode: HttpStatus.BAD_REQUEST,
+            },
+            HttpStatus.BAD_REQUEST,
+          );
+        },
       }),
     },
   ],
